1a7c67bdd82f8ad9d91d68e413723c971225c4f1
mumarch <umarch.skku@gmail.com>
2024-08-28 14:02:16 +0500

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,38 @@
+# numpy version:1.26.4 is compatable with pandasai
+# pip install numpy==1.26.4  pyyaml
+
+import os
+import pandas as pd
+import streamlit as st # type: ignore
+from pandasai import Agent
+#import numpy as np
+
+#from pandasai.llm.local_llm import LocalLLM
+from pandasai import SmartDataframe
+
+os.environ["PANDASAI_API_KEY"] = "$2a$10$fM2M9OzQKknKddp1YYRGKe/OpllKpux0yKRlZo8y2zTKBb0M/0U9a"
+
+
+st.title("Data Analysis using Prompts")
+
+uploaded_file = st.file_uploader("Upload a CSV file", type=['csv'])
+
+# reading the uploaded dataset
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write(data.head(2))
+
+    agent = Agent(data)
+
+    prompt = st.text_area("Enter your prompt")
+    
+    if st.button("Generate"):
+        if prompt:
+            with st.spinner("Generating response..."):
+                st.write(agent.chat(prompt))
+                
+
+
+
+
+
